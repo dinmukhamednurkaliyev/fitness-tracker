351a62d9158b6db27d593c7e2283c3d0a21c9cab
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-12-17 17:13:53 +0500

    feat(client-app): add base multi-platform application structure

---
+++ applications/client_app/lib/l10n/l10n.dart
@@ -0,0 +1,8 @@
+import 'package:client_app/l10n/gen/app_localizations.dart';
+import 'package:flutter/widgets.dart';
+
+export 'package:client_app/l10n/gen/app_localizations.dart';
+
+extension AppLocalizationsX on BuildContext {
+  AppLocalizations get l10n => AppLocalizations.of(this);
+}
---
+++ applications/client_app/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:client_app/app/app.dart';
+import 'package:client_app/bootstrap.dart';
+
+Future<void> main() async {
+  await bootstrap(() => const App());
+}
